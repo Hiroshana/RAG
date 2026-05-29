5ae93772eef632bb30f1fd1ceb194453a3141e72
Hiroshana <hiroshanawanni@gmail.com>
2026-05-29 19:22:44 +1000

    Initial commit

---
+++ ingestion_pipeline.py
@@ -0,0 +1,18 @@
+import os
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def main():
+    print("Main function")
+    
+    #1. loading the files
+    #2. Chunking the files
+    #3. Embedding and storing in Vector DB
+    
+if __name__ == "__main__":
+    main()
